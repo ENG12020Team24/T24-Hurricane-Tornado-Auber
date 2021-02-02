37ef29598d8c8062b790300835eb8d963e54dbe9
Daniel Allinson <danielallinson01@gmail.com>
2021-02-02 15:44:21 +0000

    Initial Checkstyle fixes in CrewMembers

--- core/src/main/java/com/mygdx/auber/entities/CrewMembers.java
+++ core/src/main/java/com/mygdx/auber/entities/CrewMembers.java
@@ -8,31 +8,41 @@ import com.mygdx.auber.Pathfinding.GraphCreator;
 import com.mygdx.auber.Pathfinding.MapGraph;
 import com.mygdx.auber.Pathfinding.Node;
 
-public class CrewMembers extends NPC {
-    public double timeToWait = Math.random() * 15;
-    public static Array<Sprite> crewSprites = new Array<>();
+public final class CrewMembers extends NPC {
+    /** How long to wait for in seconds between movements. */
+    private double timeToWait = Math.random() * 15;
+    /** The array of Sprites Crewmates can be. */
+    private static Array<Sprite> crewSprites = new Array<>();
 
-    public CrewMembers(Sprite sprite, Node node, MapGraph mapGraph) {
+    /**
+     * Class constructor.
+     * @param sprite The sprite for this crew member.
+     * @param node The node that this crew member starts on.
+     * @param mapGraph The mapGraph that this crew member uses to navigate.
+     */
+    public CrewMembers(final Sprite sprite, final Node node,
+        final MapGraph mapGraph) {
         super(sprite, node, mapGraph, Config.CREW_MEMBER_SPEED);
         this.setPosition(node.getX(), node.getY());
     }
 
     /**
-     * Step needs to be called in the update method, makes the NPC move and check if
-     * it has reached its next node
+     * Step needs to be called in the update method, makes the NPC move and 
+     * check if it has reached its next node.
      */
-    public void step(float delta) {
+    public void step(final float delta) {
         this.moveNPC(delta);
 
         this.elapsedTime += delta;
         this.checkCollision(Config.CREW_MEMBER_SPEED);
 
-        // this.collision.checkForCollision(this, layer, this.velocity, this.collision);
+        // this.collision.checkForCollision(this, layer, this.velocity,
+            // this.collision);
         // //This line enables collision, need to give same layers as player though,
         // wouldn't recommend
 
-        if ((this.elapsedTime >= timeToWait) && this.pathQueue.isEmpty()) { // If wait time has elapsed and no where
-                                                                            // else to go in path
+        if ((this.elapsedTime >= timeToWait) && this.pathQueue.isEmpty()) { 
+            // If wait time has elapsed and no where else to go in path
             this.elapsedTime = 0;
             reachDestination();
         }
@@ -50,9 +60,10 @@ public class CrewMembers extends NPC {
         double chance = Math.random();
 
         if (chance < 0.2) {
-            setGoal(GraphCreator.getKeySystemNodes().random(), Config.CREW_MEMBER_SPEED);
-        } // 1/5 chance of going to a key system
-        else {
+            setGoal(GraphCreator.getKeySystemNodes().random(),
+                Config.CREW_MEMBER_SPEED);
+            // 1/5 chance of going to a key system
+        } else {
             Node newGoal;
             do {
                 newGoal = MapGraph.getNodes().random();
@@ -75,9 +86,8 @@ public class CrewMembers extends NPC {
     }
 
     /**
-     * Returns a crew member sprite, low chance of anime
-     * 
-     * @return A Sprite
+     * Returns a crew member sprite, low chance of anime.
+     * @return A randomly selected Sprite.
      */
     public static Sprite selectSprite() {
         double chance = Math.random() * 20;
@@ -92,13 +102,20 @@ public class CrewMembers extends NPC {
         } else {
             return crewSprites.get(0);
         }
-    } // Low chance of anime sprites (Always innocent) and high chance of construction
-      // worker or alien
+    } // Low chance of anime sprites (Always innocent) and high chance of
+      // construction worker or alien
 
-    public void setIndex(int index) {
-        this.index = index;
+    /**
+     * Sets the index of this crew member
+     * @param newIndex The new index of this c.rew member.
+     */
+    public void setIndex(final int newIndex) {
+        this.index = newIndex;
     }
 
+    /**
+     * Method implemented from abstract superclass.
+     */
     public void dispose() {
         crewSprites.clear();
     }
