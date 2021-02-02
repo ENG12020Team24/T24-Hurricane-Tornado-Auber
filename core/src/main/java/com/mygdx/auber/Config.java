613098da7a97fba96da16c7503c41e6ec4711682
Daniel Allinson <danielallinson01@gmail.com>
2021-02-02 15:00:29 +0000

    Checkstyle fixes in ScrollingBackground

--- core/src/main/java/com/mygdx/auber/Config.java
+++ core/src/main/java/com/mygdx/auber/Config.java
@@ -109,4 +109,7 @@ public final class Config {
 
     /** The amount of systems that can be destroyed before the player loses. */
     public static final int MAX_LOST_SYSTEMS = 15;
+
+    /** The speed of the background scroll in pixels per second. */
+    public static final float BACKGROUND_SCROLL_SPEED = 30;
 }
--- core/src/main/java/com/mygdx/auber/ScrollingBackground.java
+++ core/src/main/java/com/mygdx/auber/ScrollingBackground.java
@@ -4,35 +4,47 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ScrollingBackground {
+    /** The background texture. */
+    private Texture image;
+    /** The first y value used in rendering. */
+    private float y1;
+    /** The second y value used in rendering. */
+    private float y2;
+    /** The x value used in rendering. */
+    private float x;
+    /** The amount to scale the background by. */
+    private float imageScale;
+    /** The initial value for x. */
+    private static final float INITIAL_X = 200;
+    /** The initial value for y1. */
+    private static final float INITIAL_Y = 2000;
+    /** The initial offset for y2. */
+    private static final float INITIAL_Y2 = 40;
 
-    Texture image;
-    float y1, y2;
-    float x;
-    public static int SPEED = 30; // In pixels per second
-    float imageScale;
-
+    /**
+     * Class constructor.
+     */
     public ScrollingBackground() {
         image = new Texture("background.png");
-
-        y1 = 2000;
-        y2 = y1 + image.getHeight() - 40;
-        x = 200;
+        y1 = INITIAL_Y;
+        y2 = y1 + image.getHeight() - INITIAL_Y2;
+        x = INITIAL_X;
         imageScale = 1;
     }
 
     /**
-     * Used to update and render the background
-     * @param delta The time in seconds since the previous frame
-     * @param batch
+     * Used to update and render the background.
+     * @param delta The time in seconds since the previous frame.
+     * @param batch The batch used to draw the background.
      */
-    public void updateRender(float delta, SpriteBatch batch) {
-        y1 -= SPEED * delta;
-        y2 -= SPEED * delta;
+    public void updateRender(final float delta, final SpriteBatch batch) {
+        y1 -= Config.BACKGROUND_SCROLL_SPEED * delta;
+        y2 -= Config.BACKGROUND_SCROLL_SPEED * delta;
 
-        if (y1 + image.getHeight() * imageScale <= 2000) {
+        if (y1 + image.getHeight() * imageScale <= INITIAL_Y) {
             y1 = y2 + image.getHeight() * imageScale;
         }
-        if (y2 + image.getHeight() * imageScale <= 2000) {
+        if (y2 + image.getHeight() * imageScale <= INITIAL_Y) {
             y2 = y1 + image.getHeight() * imageScale;
         }
 
@@ -41,11 +53,11 @@ public class ScrollingBackground {
     }
 
     /**
-     * Resets the size of the background
+     * Resets the size of the background.
      * @param width
      * @param height
      */
-    public void resize(int width, int height) {
+    public void resize(final int width, final int height) {
         imageScale = 1;
     }
 }
