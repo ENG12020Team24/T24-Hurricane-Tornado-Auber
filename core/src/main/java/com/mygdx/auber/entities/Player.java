bb4c761dbc83889f5bcfc006fc49c1840aa8b335
Daniel Allinson <danielallinson01@gmail.com>
2021-02-09 15:25:06 +0000

    Another Javadoc test, DO NOT MERGE

--- core/src/main/java/com/mygdx/auber/entities/Player.java
+++ core/src/main/java/com/mygdx/auber/entities/Player.java
@@ -160,7 +160,7 @@ public final class Player extends Sprite implements InputProcessor {
 
     /**
      * Draws the arrest radius for Auber.
-     * @param shapeRenderer
+     * @param TEST Shape renderer to be used for drawing shapes.
      */
     public void drawCircle(final ShapeRenderer shapeRenderer) {
         if (Gdx.input.getX() != Gdx.input.getX()
